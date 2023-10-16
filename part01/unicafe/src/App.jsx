8030dd5872de8a30a2e727a6468efd63eec4cb8d
zolantal <zoltan_antal@outlook.com>
2023-10-16 17:32:52 +0100

    Complete exercise 1.8

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -5,18 +5,28 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+}
+
+function Button({ onClick, text }) {
+  return <button onClick={onClick}>{text}</button>;
+}
+
+function Statistics({ good, neutral, bad }) {
   const all = good + neutral + bad;
   const average = (good - bad) / all;
   const positive = good / all;
 
   return (
-    <div>
-      <h1>give feedback</h1>
-      <div>
-        <Button onClick={() => setGood(good + 1)} text="good" />
-        <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-        <Button onClick={() => setBad(bad + 1)} text="bad" />
-      </div>
+    <>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
@@ -24,12 +34,8 @@ function App() {
       <p>all {all}</p>
       <p>average {average}</p>
       <p>positive {positive * 100} %</p>
-    </div>
+    </>
   );
-}
-
-function Button({ onClick, text }) {
-  return <button onClick={onClick}>{text}</button>;
 }
 
 export default App;
