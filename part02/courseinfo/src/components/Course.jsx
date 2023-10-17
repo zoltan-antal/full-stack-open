0064b1abd926d9146d39a4d9d9aac414b3b98f41
zolantal <zoltan_antal@outlook.com>
2023-10-17 11:14:33 +0100

    Complete exercise 2.1

---
+++ part02/courseinfo/src/components/Course.jsx
@@ -0,0 +1,14 @@
+function Course({ course }) {
+  return (
+    <>
+      <h1>{course.name}</h1>
+      {course.parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </>
+  );
+}
+
+export default Course;
