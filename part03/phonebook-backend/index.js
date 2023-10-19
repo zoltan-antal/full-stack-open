b766e558ac98db3aacba2079385c847ce549332f
zolantal <zoltan_antal@outlook.com>
2023-10-19 13:02:45 +0100

    Complete exercise 3.2

--- part03/phonebook-backend/index.js
+++ part03/phonebook-backend/index.js
@@ -24,6 +24,15 @@ const persons = [
   },
 ];
 
+app.get('/info', (request, response) => {
+  response.send(
+    `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `
+  );
+});
+
 app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
