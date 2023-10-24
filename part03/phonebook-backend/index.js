b25c00f6fd4f1dfc93b8a0c89c868ecd3ba24c91
zolantal <zoltan_antal@outlook.com>
2023-10-24 12:13:29 +0100

    Complete exercise 3.18

--- part03/phonebook-backend/index.js
+++ part03/phonebook-backend/index.js
@@ -17,12 +17,14 @@ app.use(
 );
 
 app.get('/info', (request, response) => {
-  response.send(
-    `
+  Person.find({}).then((persons) => {
+    response.send(
+      `
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
     `
-  );
+    );
+  });
 });
 
 app.get('/api/persons', (request, response) => {
