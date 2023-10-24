993aa308454db7b335252c3f24746b00c527b67a
zolantal <zoltan_antal@outlook.com>
2023-10-24 11:50:51 +0100

    Complete exercise 3.16

--- part03/phonebook-backend/index.js
+++ part03/phonebook-backend/index.js
@@ -65,6 +65,18 @@ app.post('/api/persons', (request, response) => {
   });
 });
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
