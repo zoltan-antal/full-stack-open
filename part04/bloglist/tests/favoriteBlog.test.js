5abfede190b76921b304fc141b3f802ac4a7da79
zolantal <zoltan_antal@outlook.com>
2023-10-25 13:33:29 +0100

    Amend test files syntax

--- part04/bloglist/tests/favoriteBlog.test.js
+++ part04/bloglist/tests/favoriteBlog.test.js
@@ -1,9 +1,9 @@
-const listHelper = require('../utils/list_helper');
+const { favoriteBlog } = require('../utils/list_helper');
 const { listWithOneBlog, listWithManyBlogs } = require('./blog_data');
 
 describe('favourite blog', () => {
   test('when list has only one blog, equals to that blog', () => {
-    const result = listHelper.favoriteBlog(listWithOneBlog);
+    const result = favoriteBlog(listWithOneBlog);
     expect(result).toEqual({
       title: 'Go To Statement Considered Harmful',
       author: 'Edsger W. Dijkstra',
@@ -12,7 +12,7 @@ describe('favourite blog', () => {
   });
 
   test('when list has several blogs, equals the blog with the most likes', () => {
-    const result = listHelper.favoriteBlog(listWithManyBlogs);
+    const result = favoriteBlog(listWithManyBlogs);
     expect(result).toEqual({
       title: 'Canonical string reduction',
       author: 'Edsger W. Dijkstra',
