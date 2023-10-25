866c25da974da3cd6c064d659ad4217d2829cd40
zolantal <zoltan_antal@outlook.com>
2023-10-25 13:17:24 +0100

    Complete exercise 4.4 & 4.5

--- part04/bloglist/utils/list_helper.js
+++ part04/bloglist/utils/list_helper.js
@@ -1,7 +1,24 @@
+const _ = require('lodash');
+
 const dummy = () => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, curr) => acc + curr.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce(
+    (acc, curr) => (curr.likes > acc.likes ? curr : acc),
+    blogs[0]
+  );
+
+  return _.pick(favorite, ['title', 'author', 'likes']);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
