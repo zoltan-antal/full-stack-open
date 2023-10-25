b4ba2fa0778c414919b29a3e7d2ba9ad6fdc7011
zolantal <zoltan_antal@outlook.com>
2023-10-25 13:33:29 +0100

    Amend test files syntax

--- part04/bloglist/tests/totalLikes.test.js
+++ part04/bloglist/tests/totalLikes.test.js
@@ -1,14 +1,14 @@
-const listHelper = require('../utils/list_helper');
+const { totalLikes } = require('../utils/list_helper');
 const { listWithOneBlog, listWithManyBlogs } = require('./blog_data');
 
 describe('total likes', () => {
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
+    const result = totalLikes(listWithOneBlog);
     expect(result).toBe(5);
   });
 
   test('when list has several blogs, equals the sum of all the likes', () => {
-    const result = listHelper.totalLikes(listWithManyBlogs);
+    const result = totalLikes(listWithManyBlogs);
     expect(result).toBe(36);
   });
 });
