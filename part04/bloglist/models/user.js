ca5dff39fbf33913683bb3cdf721c682b192484c
zolantal <zoltan_antal@outlook.com>
2023-10-27 16:12:40 +0100

    Complete exercise 4.16

--- part04/bloglist/models/user.js
+++ part04/bloglist/models/user.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    minLength: 3,
+    unique: true,
+  },
   passwordHash: String,
   name: String,
 });
+
+userSchema.plugin(uniqueValidator);
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
