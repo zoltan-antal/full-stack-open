f04663351b28e8b28941d0bf62b120ca3b41f4a3
zolantal <zoltan_antal@outlook.com>
2023-11-02 10:34:20 +0000

    Set up E2E testing with Cypress

--- part05/bloglist-frontend/.eslintrc.cjs
+++ part05/bloglist-frontend/.eslintrc.cjs
@@ -1,17 +1,23 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, 'jest/globals': true },
+  env: {
+    browser: true,
+    es2020: true,
+    'jest/globals': true,
+    'cypress/globals': true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:jest/recommended',
+    'plugin:cypress/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'jest'],
+  plugins: ['react-refresh', 'jest', 'cypress'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
