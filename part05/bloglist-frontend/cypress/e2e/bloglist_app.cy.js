1500d55d47523996b3f780d09a13314f8160d2e4
zolantal <zoltan_antal@outlook.com>
2023-11-02 11:09:05 +0000

    Complete exercise 5.18

--- part05/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part05/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,10 +1,41 @@
 describe('Bloglist app', () => {
+  let user;
+
   beforeEach(function () {
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`);
+    user = {
+      name: 'Example User',
+      username: 'example',
+      password: 'example',
+    };
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user);
     cy.visit('');
   });
 
   it('Login form is shown', function () {
     cy.get('form.login-form');
+  });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('form.login-form').as('loginForm');
+      cy.get('@loginForm').find('#username').type(user.username);
+      cy.get('@loginForm').find('#password').type(user.password);
+      cy.get('@loginForm').find('#login-button').click();
+      cy.contains(`${user.name} logged in`);
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('form.login-form').as('loginForm');
+      cy.get('@loginForm').find('#username').type(user.username);
+      cy.get('@loginForm').find('#password').type('wrong');
+      cy.get('@loginForm').find('#login-button').click();
+      cy.contains(`${user.name} logged in`).should('not.exist');
+      cy.get('.notification.error').should(
+        'have.css',
+        'color',
+        'rgb(255, 0, 0)'
+      );
+    });
   });
 });
--- part05/bloglist-frontend/src/App.jsx
+++ part05/bloglist-frontend/src/App.jsx
@@ -112,30 +112,6 @@ const App = () => {
     }
   };
 
-  // const loginForm = () => (
-  //   <form onSubmit={handleLogin}>
-  //     <div>
-  //       username
-  //       <input
-  //         type="text"
-  //         value={username}
-  //         name="Username"
-  //         onChange={({ target }) => setUsername(target.value)}
-  //       />
-  //     </div>
-  //     <div>
-  //       password
-  //       <input
-  //         type="password"
-  //         value={password}
-  //         name="Password"
-  //         onChange={({ target }) => setPassword(target.value)}
-  //       />
-  //     </div>
-  //     <button type="submit">login</button>
-  //   </form>
-  // );
-
   const blogFormRef = useRef();
 
   if (!user) {
