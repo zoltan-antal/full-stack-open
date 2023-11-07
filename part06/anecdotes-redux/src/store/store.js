13ab8f837bf668d4f4523b2776f3e821d3ab512d
zolantal <zoltan_antal@outlook.com>
2023-11-07 12:29:01 +0000

    Complete exercise 6.12

--- part06/anecdotes-redux/src/store/store.js
+++ part06/anecdotes-redux/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import anectodeReducer from '../reducers/anecdoteReducer';
 import filterReducer from '../reducers/filterReducer';
+import notificationReducer from '../reducers/notificationReducer';
 
 const store = configureStore({
   reducer: {
     anecdotes: anectodeReducer,
     filter: filterReducer,
+    notification: notificationReducer,
   },
 });
 
