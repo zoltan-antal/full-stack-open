41e8f7ee3b33bb47691094bdd1aabcd619235a3d
zolantal <zoltan_antal@outlook.com>
2023-11-20 14:28:50 +0000

    Complete exercise 8.6

--- part08/library-backend/index.js
+++ part08/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { v1: uuid } = require('uuid');
 
 let authors = [
   {
@@ -100,6 +101,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `;
 
 const resolvers = {
@@ -127,6 +137,18 @@ const resolvers = {
     books: (root) => books.filter((book) => book.author === root.name),
     bookCount: (root) =>
       books.filter((book) => book.author === root.name).length,
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      books = [...books, book];
+      if (!authors.find((author) => author.name === book.author)) {
+        const author = { name: book.author, id: uuid() };
+        authors = [...authors, author];
+      }
+      return book;
+    },
   },
 };
 
