07a3edd570d083b5573918682adfe6a522555f2f
zolantal <zoltan_antal@outlook.com>
2023-11-20 14:34:23 +0000

    Complete exercise 8.7

--- part08/library-backend/index.js
+++ part08/library-backend/index.js
@@ -109,6 +109,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `;
 
@@ -148,6 +152,19 @@ const resolvers = {
         authors = [...authors, author];
       }
       return book;
+    },
+
+    editAuthor: (root, args) => {
+      const author = authors.find((author) => author.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((author) =>
+        author.name === args.name ? updatedAuthor : author
+      );
+      return updatedAuthor;
     },
   },
 };
