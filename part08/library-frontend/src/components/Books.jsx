a900708cfcde8ec00ad3cdd5fda089b1ff50ae09
zolantal <zoltan_antal@outlook.com>
2023-11-21 12:42:39 +0000

    Complete exercise 8.9

--- part08/library-frontend/src/components/Books.jsx
+++ part08/library-frontend/src/components/Books.jsx
@@ -1,5 +1,14 @@
+import { useQuery } from '@apollo/client';
+import { ALL_BOOKS } from '../queries';
+
 const Books = () => {
-  const books = [];
+  const result = useQuery(ALL_BOOKS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  const books = result.data.allBooks;
 
   return (
     <div>
