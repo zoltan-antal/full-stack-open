9a9886ec8637ffaf95fd5bfa9c883c0dc53e912c
zolantal <zoltan_antal@outlook.com>
2023-11-28 17:14:25 +0000

    Complete exercise 9.11

--- part09/patientor-backend/src/index.ts
+++ part09/patientor-backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import diagnosisRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
 
 const app = express();
 app.use(express.json());
@@ -10,8 +11,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
-
 app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
---
+++ part09/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,18 @@
+import patients from '../../data/patients';
+import { NonSensitivePatientEntry, PatientEntry } from '../types';
+
+const getEntries = (): PatientEntry[] => {
+  return patients;
+};
+
+const getNonSensitiveEntries = (): NonSensitivePatientEntry[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
+export default { getEntries, getNonSensitiveEntries };
