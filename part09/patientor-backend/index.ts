a91d24f0dd2c2e671ab0657865976367ef4a9e8b
zolantal <zoltan_antal@outlook.com>
2023-11-28 15:17:49 +0000

    Complete exercise 9.8

---
+++ part09/patientor-backend/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
