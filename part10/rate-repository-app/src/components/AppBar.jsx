92ba51b3420ca0a7e8e0df56b342740f977e28c7
zolantal <zoltan_antal@outlook.com>
2023-12-04 17:59:43 +0000

    Complete exercise 10.7

--- part10/rate-repository-app/src/components/AppBar.jsx
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
@@ -15,8 +15,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab text={'Repositories'} route={'/'} />
-      <AppBarTab text={'Sign in'} route={'/signIn'} />
+      <ScrollView horizontal>
+        <AppBarTab text={'Repositories'} route={'/'} />
+        <AppBarTab text={'Sign in'} route={'/signIn'} />
+      </ScrollView>
     </View>
   );
 };
