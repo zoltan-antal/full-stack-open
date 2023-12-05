dc0042dbea9ca084f0b1a8400e59837c6ca5064b
zolantal <zoltan_antal@outlook.com>
2023-12-05 16:45:07 +0000

    Complete exercise 10.12

--- part10/rate-repository-app/src/utils/apolloClient.js
+++ part10/rate-repository-app/src/utils/apolloClient.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
-  uri: 'http://192.168.178.29:4000/graphql',
+  uri: Constants.expoConfig.extra.apolloUri,
 });
 
 const createApolloClient = () => {
