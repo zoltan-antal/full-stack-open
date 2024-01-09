82571094297f93ffea3f6d2dfcd1981c8b8f6d04
zolantal <zoltan_antal@outlook.com>
2024-01-09 14:58:03 +0000

    Complete exercise 12.9

--- part12/todo-app/todo-backend/routes/index.js
+++ part12/todo-app/todo-backend/routes/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const configs = require('../util/config')
+const redis = require('../redis');
 
-let visits = 0
+const configs = require('../util/config');
+
+let visits = 0;
 
 /* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
   });
 });
 
