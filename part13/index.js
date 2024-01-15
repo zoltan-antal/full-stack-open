b6f955c14826c30766a4a1cc57b2fb472d2f647e
zolantal <zoltan_antal@outlook.com>
2024-01-15 15:29:37 +0000

    Complete exercise 13.4

---
+++ part13/index.js
@@ -0,0 +1,72 @@
+require('dotenv').config();
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {},
+});
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+);
+Blog.sync();
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    return res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await blog.destroy();
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
