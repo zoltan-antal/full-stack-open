6f754b66e804d0277360b6b52d8542764a120d87
zolantal <zoltan_antal@outlook.com>
2024-01-15 16:09:15 +0000

    Complete exercises 13.5 & 13.6

---
+++ part13/controllers/blogs.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+
+const { Blog } = require('../models');
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    return res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
+
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    res.json(req.blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    blog = req.blog;
+    blog.likes = req.body.likes;
+    await blog.save();
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
