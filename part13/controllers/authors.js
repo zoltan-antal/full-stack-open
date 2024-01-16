6aab8b67194734e9f8d5518dee905fe0ea2f472a
zolantal <zoltan_antal@outlook.com>
2024-01-16 15:33:03 +0000

    Complete exercise 13.16

---
+++ part13/controllers/authors.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { sequelize } = require('../utils/db');
+
+const { Blog } = require('../models');
+
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('*')), 'articles'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes'],
+    ],
+    group: ['author'],
+    order: [['likes', 'DESC']],
+  });
+  res.json(authors);
+});
+
+module.exports = router;
