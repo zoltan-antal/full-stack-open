24f293485e308698b0ceea8691aa86b1de8411ae
zolantal <zoltan_antal@outlook.com>
2024-01-16 14:56:13 +0000

    Complete exercise 13.14

--- part13/controllers/blogs.js
+++ part13/controllers/blogs.js
@@ -28,9 +28,18 @@ router.get('/', async (req, res) => {
   const where = {};
 
   if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`,
-    };
+    where[Op.or] = [
+      {
+        title: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+      },
+      {
+        author: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+      },
+    ];
   }
 
   const blogs = await Blog.findAll({
