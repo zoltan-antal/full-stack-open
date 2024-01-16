3a0c9070d249a2d515864cd468b759a6819c5f70
zolantal <zoltan_antal@outlook.com>
2024-01-16 12:36:32 +0000

    Complete exercise 13.8

--- part13/app.js
+++ part13/app.js
@@ -3,10 +3,14 @@ const app = express();
 require('express-async-errors');
 const middleware = require('./utils/middleware');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
---
+++ part13/controllers/users.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+const { User } = require('../models');
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.put('/:username', async (req, res) => {
+  const user = await User.findOne({ where: { username: req.params.username } });
+  if (!user) {
+    res.status(404).end();
+  }
+  if (!req.body.username) {
+    res.status(400).end();
+  }
+  console.log(req.body.username);
+  user.username = req.body.username;
+  user.save();
+  res.json(user);
+});
+
+module.exports = router;
